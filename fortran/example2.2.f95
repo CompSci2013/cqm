d65e7a497bda153fca7952ae83727563fca8b2cd
CompSci2013 <kcollins2004@gmail.com>
2023-10-16 06:23:20 -0400

    Add temperature example program.

---
+++ fortran/example2.2.f95
@@ -0,0 +1,18 @@
+program temperature
+    implicit none
+    
+    ! declare variables
+    real :: DegC, DegF
+
+    write(*,*)"Please type in temp in Celcius"
+    ! the read statement reads input from the keyboard,
+    ! and stores it in variable DecC
+    read(*,*)DegC
+
+    !Comment line below to see what happens with undefined variables
+    DegF = (9./5.)*DegC + 32.
+
+    ! The write statement accepts multiple
+    ! strings or variables, separated by commas
+    write(*,*)"This equals to",DegF,"Fahrenheit"
+end program temperature
